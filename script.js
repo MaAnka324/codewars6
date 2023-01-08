88f42da8227474d1e3bcae893dd9496b78d2f80b
MaAnka324 <115251389+MaAnka324@users.noreply.github.com>
2023-01-08 13:50:46 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+///////////////////////////////Get the Middle Character (7kyu)
+function getMiddle(s)
+{
+  //Code goes here!
+  if(s.length%2==0){
+    return s[Math.ceil(s.length/2, 1) - 1] + s[Math.ceil(s.length/2, 1)]
+  }
+  else {
+    return s[Math.ceil(s.length/2, 1) - 1]
+  }
+}
